1fa56616251b4917346c9f12c4bc5047e7c71689
MA QIANCHENG <mqcw@163.com>
2023-09-28 09:50:12 +0900

    投げ槍の実装

---
+++ Assets/Scripts/Weapon/WoodenSpearView/WoodenSpearController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodenSpearController : WeaponControllerBase
+{
+    private WoodenSpearView m_WoodenSpearView;
+    public override void Init(){
+        m_WoodenSpearView = (WoodenSpearView)M_WeaponViewBase;
+        ReadyToFire(0);
+
+    }
+
+    public override void LoadAsset(){
+        AudioClip = Resources.Load<AudioClip>("Audio/Weapon/Arrow_Release");
+
+    }
+
+    public override void Shoot(){
+    }
+
+    public override void PlayFireEffect(){
+        //エフェクトなし
+    }
+
+    public override void PlayFireAnimation(){
+        //アニメーションなし
+
+    }
+
+}
---
+++ Assets/Scripts/Weapon/WoodenSpearView/WoodenSpearView.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodenSpearView : WeaponViewBase
+{
+    public override void Init(){
+        M_MuzzlePos = gameObject.transform.Find("Armature/Arm_R/Forearm_R/Wrist_R/Weapon/Pos_Muzzle");
+    }
+
+    public override void CrosshairInit(){
+        M_Crosshair = GameObject.Find("Crosshair").gameObject.transform;
+    }
+
+    public override void HoldPoseInit(){
+        M_StartPos = transform.localPosition;
+        M_StartRot = transform.localRotation.eulerAngles;
+        M_EndPos = new Vector3(0.0f, -1.55f, 0.32f);
+        M_EndRot = new Vector3(0f, 3.9f, 0.3f);
+    }
+
+    public override void HoldPoseStart(){
+        transform.DOLocalMove(M_EndPos, 0.2f);
+        transform.DOLocalRotate(M_EndRot, 0.2f);
+        M_EnvCamera.DOFieldOfView(40, 0.2f);
+    }
+
+    public override void HoldPoseEnd(){
+        transform.DOLocalMove(M_StartPos, 0.2f);
+        transform.DOLocalRotate(M_StartRot, 0.2f);
+
+        M_EnvCamera.DOFieldOfView(60, 0.2f);
+    }
+
+
+}
