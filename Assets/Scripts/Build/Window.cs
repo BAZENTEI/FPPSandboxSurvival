f79a7ffdc78244e8686515e3d856e9fd587f45a3
MA QIANCHENG <mqcw@163.com>
2024-02-04 17:49:52 +0900

    窓の実装

---
+++ Assets/Scripts/Build/Window.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MaterialModelBase
+{
+
+    protected override void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.name == "windowTrigger"){
+            IsCanPut = true;
+            IsAttach = true;
+            transform.position = coll.gameObject.transform.position;
+            transform.rotation = coll.gameObject.transform.rotation;
+        }
+    }
+
+    protected override void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.name == "windowTrigger"){
+            IsCanPut = false;
+            IsAttach = false;
+        }
+    }
+}
